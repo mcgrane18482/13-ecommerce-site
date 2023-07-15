45ce0c044a4b1ab725aa368e385762c1c2c4bdaa
Fruit Juice <c.erinmcgrane1@gmail.com>
2023-07-15 17:29:30 -0400

    added README file

---
+++ README.md
@@ -0,0 +1,11 @@
+# Backend for E-Commerce Site
+
+## Description 
+This directory is used to create, seed, and perform CRUD operations for an ecommerce database.
+
+## Credits
+Routes for creating a new product and updating a product were provided by Rutgers Full Stack Flex Bootcamp in July of 2023.
+
+## Preview
+Video demonstration can be viewed [here](https://drive.google.com/file/d/17sB7yU-tvzDQlYGBFyq5e4Pp8otg_KRj/view)
+
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -60,13 +60,14 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id
       }
     });
+    // console.log(category);
+    if (category === 0) {
+      res.status(404).json({ error: 'No category was found with that id' });
+      return;
+    } else {
+      res.status(200).json({ message: 'Category successfully deleted' })
 
-    if (!category) {
-      return res.status(404).json({ error: 'No category was found with that id' });
-    };
-
-    res.status(200).json({ message: 'Category successfully deleted' })
-
+    }
   } catch (err) {
     res.status(500).json(err);
   }
