956b7b68612ff879ea16afda91cd6eb2678bee4f
Fruit Juice <c.erinmcgrane1@gmail.com>
2023-07-14 20:57:44 -0400

    coded all routes except for deleting a product

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,57 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const categories = await Category.findAll({include: Product});
+
+  res.send(categories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const category = await Category.findOne({
+    where: {tag: req.params.id}
+  });
+
+  res.send(category);
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const new_Category = await Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name
+  })
+
+  res.send(new_Category);
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const category_id = req.params.id;
+
+  const category = await Category.update(req.body, { where: {id: category_id}})
+
+  res.json(category);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const category = await Category.findByPk(req.params.id);
+
+  if(!category) {
+    return res.status(404).json({error: 'Category not found'});
+  };
+
+  await category.destroy();
+
+  res.status(200).json({message: 'Category successfully deleted'})
+
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,57 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  const tags = await Tag.findAll({
+    include: Product
+  });
+  res.send(tags);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const tag_id = req.params.id;
+
+  const tag = await Tag.findOne({ include: Product }, { where: { id: tag_id } })
+
+  res.send(tag);
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  const new_tag = await Tag.create({
+    id: req.body.id,
+    tag_name: req.body.tag_name
+  });
+
+  res.send(new_tag);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  const tag_id = req.params.id;
+
+  const tag = await Tag.update(req.body, { where: { id: tag_id } })
+
+  res.json(tag);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  const tag = await Tag.findByPk(req.params.id);
+
+  if (!tag) {
+    return res.status(404).json({error: 'Tag not found'});
+  }
+
+  await tag.destroy();
+
+  res.status(200).json({message:'Tag successfully deleted'});
+
 });
 
 module.exports = router;
